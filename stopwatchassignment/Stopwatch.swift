67589aad64e46e3a019ec14de815965f66f78359
Kunal Dandona <kunaldandona009@gmail.com>
2019-01-19 13:43:02 -0800

    Test

--- stopwatchassignment/Stopwatch.swift
+++ stopwatchassignment/Stopwatch.swift
@@ -3,7 +3,7 @@
 //  stopwatchassignment
 //
 //  Created by Kunal Dandona on 2019-01-19.
-//  Copyright © 2019 Kunal Dandona. All rights reserved.
+//  Copyright © 2019- Kunal Dandona. All rights reserved.
 //
 
 import Foundation
